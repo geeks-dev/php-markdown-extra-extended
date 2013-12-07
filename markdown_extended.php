c1be30d6a654544a3948c52e4e35a86b89ed015a
kaz <info@geeks-dev.com>
2013-12-07 18:42:41 +0900

    google-code-prettify対応

--- markdown_extended.php
+++ markdown_extended.php
@@ -102,7 +102,7 @@ class MarkdownExtraExtended_Parser extends MarkdownExtra_Parser {
 			array(&$this, '_doFencedCodeBlocks_newlines'), $codeblock);
 		//$codeblock = "<pre><code>$codeblock</code></pre>";
 		//$cb = "<pre><code";
-		$cb = empty($matches[3]) ? "<pre><code" : "<pre class=\"linenums:$matches[3]\"><code"; 
+		$cb = empty($matches[3]) ? "<pre><code" : "<pre class=\"prettyprint linenums:$matches[3]\"><code"; 
 		$cb .= empty($matches[2]) ? ">" : " class=\"language-$matches[2]\">";
 		$cb .= "$codeblock</code></pre>";
 		return "\n\n".$this->hashBlock($cb)."\n\n";
